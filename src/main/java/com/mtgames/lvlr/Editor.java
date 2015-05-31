f0305622ebe7bd6921cf839998ea91de9d95e9e3
Dreaded_X <tim.huizinga@gmail.com>
2015-05-31 18:35:15 +0200

    Initial commit

---
+++ src/main/java/com/mtgames/lvlr/Editor.java
@@ -0,0 +1,6 @@
+package com.mtgames.lvlr;
+
+import com.mtgames.platformer.Game;
+
+public class Editor extends Game {
+}
---
+++ src/main/java/com/mtgames/lvlr/Launcher.java
@@ -0,0 +1,33 @@
+package com.mtgames.lvlr;
+
+import com.mtgames.platformer.Game;
+import com.mtgames.platformer.debug.Command;
+import com.mtgames.utils.LauncherBase;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ComboBox;
+
+public class Launcher extends LauncherBase{
+
+	static private ComboBox<Integer> scale;
+
+	public static void main(String[] args) {
+		consoleEnabled = true;
+
+		scale = new ComboBox<>(FXCollections.observableArrayList(1, 2));
+		scale.setValue(2);
+
+		addOption("Scale", scale);
+
+		launch(args);
+	}
+
+	@Override protected void run() {
+		System.setProperty("com.mtgames.scale", String.valueOf(scale.getValue()));
+
+		Game.main(new String[] { "" });
+	}
+
+	@Override protected void consoleCommand(String s) {
+		Command.exec(s);
+	}
+}
